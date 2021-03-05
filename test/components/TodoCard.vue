6891b40b249e889050bd48f18ac0fa284efd09ec
Ricard Fabien <ricard.fabienpro92@yahoo.fr>
2021-03-05 17:24:44 +0100

    Première version des cartes sans sauvegarde

---
+++ test/components/TodoCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div :idTodo=idTodo>
+        <p>Titre</p>
+        <textarea v-on:change ="$emit('update:title',newTitle)"></textarea>
+        <p>Description</p>
+        <textarea v-model="description"></textarea>
+        <p>{{date}}</p>
+        <button v-on:click="$emit('removeCard',idTodo)">Supprimer</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"TodoCard",
+    props:["idTodo","title","description","date"]
+}
+</script>
---
+++ test/pages/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <button @click ="addCard">Add</button>
+    <hr/>
+    <todo-card v-for="card in cards" :key="card.id" v-bind:idTodo.sync="card.idTodo" 
+    v-bind:title.sync="card.title" v-bind:description.sync="card.description" 
+    v-bind:date.sync="card.date" v-on:removeCard="removeCard"/>
+
+
+    
+  </div>
+</template>
+
+<script>
+import TodoCard from '../components/TodoCard.vue';
+
+export default{
+  components:{
+    TodoCard
+  },
+  data(){
+    console.log("IT'S ALIVE !");
+    return {
+      cards : [
+        {idTodo:0,title:"titreTest",description:"Description test", date: "Date test"}
+      ]
+    }
+      
+  },
+  methods:{
+    addCard(){
+        this.cards.push({
+        id:this.cards.length === 0?0:this.cards[this.cards.length-1].id+1,
+        title:"",
+        description:"",
+        date: Date.now()
+        })
+    },
+    removeCard(idTodo){
+      this.cards.splice(
+        this.cards.indexOf(
+          this.cards.find(elem => elem.id === idTodo )
+        ), 1
+      )
+    },
+    changeTitle(title){
+      console.log(title);
+    }
+  }
+}
+
+
+
+
+
+</script>
