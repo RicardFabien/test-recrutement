5d84df23bfc38bb496fd96bf6ecba32b95d71a7b
Ricard Fabien <ricard.fabienpro92@yahoo.fr>
2021-03-09 12:17:57 +0100

    Adding the test

--- test/pages/index.vue
+++ test/pages/index.vue
@@ -1,56 +1,86 @@
 <template>
   <div>
-    <button @click ="addCard">Add</button>
+    <v-btn @click ="addCard">Add</v-btn>
     <hr/>
-    <todo-card v-for="card in cards" :key="card.id" v-bind:idTodo.sync="card.idTodo" 
+    <todo-card v-for="card in cards" :key="card.id" v-bind:idTodo="card.idTodo"
     v-bind:title.sync="card.title" v-bind:description.sync="card.description" 
-    v-bind:date.sync="card.date" v-on:removeCard="removeCard"/>
-
-
-    
+    v-bind:date.sync="card.date" v-on:removeCard="removeCard" 
+    v-on:changeDesc="changeDescription(card.id, $event)" v-on:changeTitle="changeTitle(card.id,$event)"/>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import TodoCard from '../components/TodoCard.vue';
+
+interface Card{
+  id:number,
+  title : string,
+  description: string,
+  date : string
+
+}
 
 export default{
   components:{
     TodoCard
   },
   data(){
-    console.log("IT'S ALIVE !");
+    var storage = window.localStorage;
+    var cards : Array<Card> | null = JSON.parse(storage.getItem("cards"));
+    if (cards === null) 
+      cards = [];
     return {
-      cards : [
-        {idTodo:0,title:"titreTest",description:"Description test", date: "Date test"}
-      ]
+      cards : cards
     }
       
   },
   methods:{
-    addCard(){
-        this.cards.push({
-        id:this.cards.length === 0?0:this.cards[this.cards.length-1].id+1,
-        title:"",
-        description:"",
-        date: Date.now()
-        })
+    addCard(): void{
+
+        let newCard : Card = {
+          id:this.cards.length === 0?0:this.cards[this.cards.length-1].id+1,
+          title:"",
+          description:"",
+          date: new Date().toString()
+        }
+
+        this.cards.push(newCard);
+        this.modifyCards();;
     },
-    removeCard(idTodo){
+
+    removeCard(idTodo : number): void{
       this.cards.splice(
         this.cards.indexOf(
           this.cards.find(elem => elem.id === idTodo )
         ), 1
-      )
+      );
+      this.modifyCards();
     },
-    changeTitle(title){
-      console.log(title);
+
+    modifyCards(): void{
+      localStorage.setItem("cards",JSON.stringify(this.cards));
+    },
+    changeTitle(idTodo : number,title :string): void{
+      let card : Card = this.cards.find(elem => elem.id === idTodo );
+      card.title = title;
+
+      this.changeCard(idTodo)
+    }
+    ,
+    changeDescription(idTodo: number,description: string): void{
+      let card : Card = this.cards.find(elem => elem.id === idTodo );
+      card.description = description;
+
+      this.changeCard(idTodo)
+    },
+
+    changeCard(idTodo: number): void{
+      let card : Card = this.cards.find(elem => elem.id === idTodo );
+      card.date = new Date().toString();
+
+      this.modifyCards(idTodo);
+
     }
   }
 }
-
-
-
-
-
 </script>
