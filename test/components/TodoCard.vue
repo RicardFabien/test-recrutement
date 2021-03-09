1cb37b6e4ce4e81a30e1ff106baea07be00c6c1b
Ricard Fabien <ricard.fabienpro92@yahoo.fr>
2021-03-09 12:23:44 +0100

    TodoCard Warning branch

--- test/components/TodoCard.vue
+++ test/components/TodoCard.vue
@@ -1,11 +1,38 @@
 <template>
     <div :idTodo=idTodo>
-        <p>Titre</p>
-        <textarea v-on:change ="$emit('update:title',newTitle)"></textarea>
-        <p>Description</p>
-        <textarea v-model="description"></textarea>
-        <p>{{date}}</p>
-        <button v-on:click="$emit('removeCard',idTodo)">Supprimer</button>
+        
+        <v-row>
+            <v-col
+                cols="12"
+                sm="6"
+                md="4"
+            >
+                <v-text-field label="Title" v-model="title" 
+                v-on:change ="$emit('changeTitle',title)"></v-text-field>
+
+                <p>{{date}}</p>
+            </v-col> 
+
+            <v-col
+                col = "12"
+                sm = "6"
+                md = "4"
+            >
+                <v-textarea label="Description" outlined v-model="description" 
+                v-on:change ="$emit('changeDesc',description)"></v-textarea>
+            </v-col>
+
+            <v-col
+                col = "12"
+                sm = "6"
+                md = "2"
+            >
+                <v-btn style="background-color: rgb(212, 60, 0); border-color: rgb(212, 60, 0);" 
+                v-on:click="$emit('removeCard',idTodo)">Supprimer</v-btn>
+
+            </v-col>
+
+        </v-row>
     </div>
 </template>
 
